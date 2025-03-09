636903056d07243a846063d2ee926361d2339480
Marek Sýkorka <marcopolo1234565@gmail.com>
2025-03-09 10:53:27 +0100

    Added template of initialize app resources logic.

---
+++ lib/services/initialize_service.dart
@@ -0,0 +1,30 @@
+import 'internal/initialize_service_internal.dart';
+
+/// Public API for InitializeService
+/// Public API for LoggerService
+class InitResourcesService {
+  // Singleton of this class - constructed by internal constructor
+  static final InitResourcesService _instance = InitResourcesService._internal();
+  static final InitResourcesServiceInternal _internalService = InitResourcesServiceInternal();
+
+  // External "Constructor" always returns single instance of this class
+  factory InitResourcesService() => _instance;
+
+  // Internal "Constructor" initializes single instance of this class
+  InitResourcesService._internal() {
+    _internalService.initialize();
+  }
+
+  // Public Methods
+  bool isFooPresent() {
+    ///
+    /// Implement some method from InitResourcesServiceInternal
+    ///
+    throw UnimplementedError();
+  }
+
+  ///
+  /// Add other meaningful methods that will return state of peripherals or other in app components
+  ///
+
+}
---
+++ lib/services/internal/initialize_service_internal.dart
@@ -0,0 +1,15 @@
+class InitResourcesServiceInternal {
+  ///
+  /// create any number of variables needed to store initialization state pf peripherals
+  ///
+
+  initialize() async {
+    ///
+    /// Do all initialization thing_a_magick here
+    ///
+  }
+
+  ///
+  /// Create custom methods to help public API level of this service
+  ///
+}
