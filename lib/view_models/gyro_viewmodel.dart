18787d01a2893a731cd2ac75a274d834daa5b145
Marek Sýkorka <marcopolo1234565@gmail.com>
2025-04-08 12:26:21 +0200

    Fixed GraphViewModels to have sensor initialization in separate method and to implement sensor de-init in onClose() method.

--- lib/view_models/gyro_viewmodel.dart
+++ lib/view_models/gyro_viewmodel.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:rehab_app/services/models/sensor_models.dart';
-import 'package:rehab_app/services/sensor_service.dart';
+import 'package:rehab_app/models/sensor_models.dart';
+import 'package:rehab_app/services/external/sensor_service.dart';
 
 class GyroViewModel extends ChangeNotifier {
   late List<ImuSensorData> imuData;
-  late SensorService service;
   int index = 0;
 
   GyroViewModel() {
-    service = SensorService();
     imuData = List.empty(growable: true);
-    service.startGyroDataStream(samplingPeriod: Duration(milliseconds: 50));
-    service.registerGyroDataStream(callback: onDataChanged);
+  }
+
+  void registerSensorService() {
+    SensorService().startGyroDataStream(samplingPeriod: Duration(milliseconds: 50));
+    SensorService().registerGyroDataStream(callback: onDataChanged);
   }
 
   void onDataChanged() {
     if (imuData.length > 100) {
       imuData.removeAt(0);
     }
-    imuData.add(service.getGyroData());
+    imuData.add(SensorService().getGyroData());
     index++;
     notifyListeners();
+  }
+
+  void onClose() {
+    SensorService().stopGyroDataStream();
+    imuData = List.empty(growable: true);
   }
 }
--- lib/view_models/lux_viewmodel.dart
+++ lib/view_models/lux_viewmodel.dart
@@ -1,19 +1,20 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:rehab_app/services/models/sensor_models.dart';
-import 'package:rehab_app/services/sensor_service.dart';
+import 'package:rehab_app/models/sensor_models.dart';
+import 'package:rehab_app/services/external/sensor_service.dart';
 
 class LuxViewModel extends ChangeNotifier {
   late List<LuxSensorData> luxData;
-  late SensorService service;
   int index = 0;
   Timer? timer;
 
   LuxViewModel() {
-    service = SensorService();
     luxData = List.empty(growable: true);
-    service.startLuxDataStream();
-    service.registerLuxDataStream(callback: onDataChanged);
+  }
+
+  void registerSensorService() {
+    SensorService().startLuxDataStream();
+    SensorService().registerLuxDataStream(callback: onDataChanged);
     timer = Timer.periodic(const Duration(milliseconds: 50), updateUI);
   }
 
@@ -21,7 +22,7 @@ class LuxViewModel extends ChangeNotifier {
     if (luxData.length > 100) {
       luxData.removeAt(0);
     }
-    luxData.add(service.getLuxData());
+    luxData.add(SensorService().getLuxData());
     index++;
     notifyListeners();
   }
@@ -30,9 +31,20 @@ class LuxViewModel extends ChangeNotifier {
     if (luxData.length > 100) {
       luxData.removeAt(0);
     }
-    LuxSensorData data = service.getLuxData();
-    luxData.add(LuxSensorData(lux: data.lux, timeStamp: DateTime.now(), state: data.state));
+    LuxSensorData data = SensorService().getLuxData();
+    luxData.add(
+      LuxSensorData(
+          lux: data.lux,
+          timeStamp: DateTime.now(),
+          state: data.state
+      )
+    );
     index++;
     notifyListeners();
+  }
+
+  void onClose() {
+    SensorService().stopLuxDataStream();
+    luxData = List.empty(growable: true);
   }
 }
