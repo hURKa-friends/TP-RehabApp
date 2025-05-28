3538e9216990d8765ccd2d04a09b9bf345e871fd
Marek Sýkorka <marcopolo1234565@gmail.com>
2025-05-28 17:44:38 +0200

    Tweaked the project structure after PR#8

--- lib/view_models/menu_view_model.dart
+++ lib/view_models/menu_view_model.dart
@@ -47,33 +47,39 @@ class MenuViewModel extends ChangeNotifier {
               heading: 'Info',
               description: 'Táto aplikácia slúži na detekciu otrasov,'
                           ' nárazov a meranie aktivity počas jedenia.',
+              function: () {  },
             ),
             TutorialStep(
               assetURI: 'assets/eating_rehab/secure_phone.png',
               heading: '1. Krok',
               description: 'Pripevnite si Váš smartphone na zápästie '
                           'ruky, ktorou budete jesť. ',
+              function: () {  },
             ),
             TutorialStep(
               assetURI: 'assets/eating_rehab/start.png',
               heading: '2. Krok',
               description: 'Stlačte Štart a položte ruku na stôl '
                           'meranie sa spustí po 3 sekundách.',
+              function: () {  },
             ),
             TutorialStep(
               assetURI: 'assets/eating_rehab/eat_spoon.png',
               heading: '3. Krok',
               description: 'Začnite jesť.',
+              function: () {  },
             ),
             TutorialStep(
               assetURI: 'assets/eating_rehab/stop.png',
               heading: '4. Krok',
               description: 'Stlačte tlačidlo Stop na zastavenie merania.',
+              function: () {  },
             ),
             TutorialStep(
               assetURI: 'assets/eating_rehab/ok.png',
               heading: 'Potvrdenie',
               description: 'Potvrďte porozumenie. ',
+              function: () {  },
             ),
           ],
         )
@@ -91,6 +97,7 @@ class MenuViewModel extends ChangeNotifier {
                   assetURI: 'assets/pose_detection/shoulder_abduction_active.jpg',
                   heading: 'Upaženie',
                   description: 'Zdvihnite vystretú ruku bokom do horizontálnej polohy. Počas celého cviku sa snažte mať vystretú ruku s dlaňou smerujúcou nadol. Opakovanie sa ráta ako úspešné len s riadne vystretou rukou. Cvik vykonávajte pomaly.',
+                  function: () {  },
                 ),
               ],
             ),
@@ -101,6 +108,7 @@ class MenuViewModel extends ChangeNotifier {
                   assetURI: 'assets/pose_detection/shoulder_forward_elevation_active.jpg',
                   heading: 'Vzpaženie',
                   description: 'Pomaly zdvíhajte vystretú ruku pred seba a následne pokračujte až smerom ku stropu, kým vaša ruka nie je rovnobežná s vaším telom..',
+                  function: () {  },
                 ),
               ],
             ),
@@ -115,21 +123,25 @@ class MenuViewModel extends ChangeNotifier {
           assetURI: 'assets/example/FMV/beer1.png',
           heading: '1. Krok',
           description: 'Pred Vami sa zobrazí pohár piva. Po zmagnutí start sa začne odpočítavanie. Po 3 sekundách začne cvičenie.',
+          function: () {  },
         ),
         TutorialStep(
           assetURI: 'assets/example/FMV/beer2.png',
           heading: '2. Krok',
           description: 'Vašou úlohou je pomaly a plynulo preliať toto virtuálne pivo do reálneho pohára. Tento bod je dôležitý aby sa vedeli zaznamenať prípadné nárazy na hranu.',
+          function: () {  },
         ),
         TutorialStep(
           assetURI: 'assets/example/FMV/beer3.png',
           heading: '3. Krok',
           description: 'Cvičenie môžete vykonávať ohýbaním v zápästí alebo...',
+          function: () {  },
         ),
         TutorialStep(
           assetURI: 'assets/example/FMV/beer4.png',
           heading: 'Potvrdenie',
           description: '...ohýbaním v predloktí.',
+          function: () {  },
         ),
       ],
     ),
@@ -139,26 +151,31 @@ class MenuViewModel extends ChangeNotifier {
           assetURI: 'assets/example/FMV/fmv1.png',
           heading: '1. Krok',
           description: 'Najprv si vyberiete aký úchyt mobilu zvolíte. Sú na výber 4.',
+          function: () {  },
         ),
         TutorialStep(
           assetURI: 'assets/example/FMV/fmv2.png',
           heading: '2. Krok',
           description: 'Následne určíte ktorou rukou idete cvičenie vykonávať.',
+          function: () {  },
         ),
         TutorialStep(
           assetURI: 'assets/example/FMV/fmv3.png',
           heading: '3. Krok',
           description: 'Pred vami sa zobrazí obrazovka s tlačídlom zapnúť senzory. Po jeho kliknutí sa začne časomiera 5 sekúnd na položenie mobilu do šálky obrátene.',
+          function: () {  },
         ),
         TutorialStep(
             assetURI: 'assets/example/FMV/fmv4.png',
             heading: '4. Krok',
-            description: 'Cvičenie sa začne v momente keď uplynie 5 sekúnd a vy sa dotknete panelu pre dotyk s obrazovkou.'
+            description: 'Cvičenie sa začne v momente keď uplynie 5 sekúnd a vy sa dotknete panelu pre dotyk s obrazovkou.',
+            function: () {  },
         ),
         TutorialStep(
             assetURI: 'assets/example/FMV/fmv5.png',
             heading: '5. Krok',
-            description: 'Teraz presuniete zariadenie do iného hrnčeka, pomalým a plynulým pohybom a jemne položíte. Cvičenie sa skončí po uvoľnení displaya.'
+            description: 'Teraz presuniete zariadenie do iného hrnčeka, pomalým a plynulým pohybom a jemne položíte. Cvičenie sa skončí po uvoľnení displaya.',
+            function: () {  },
         ),
       ],
     ),
@@ -170,26 +187,34 @@ class MenuViewModel extends ChangeNotifier {
         TutorialStep(
             assetURI: "assets/knee_rehab/leg_extensions01.jpg",
             heading: "Step 1",
-            description: "Sit upright on a chair or bench with your back straight and feet flat on the floor."),
+            description: "Sit upright on a chair or bench with your back straight and feet flat on the floor.",
+            function: () {  },
+        ),
         TutorialStep(
             assetURI: "assets/knee_rehab/leg_extensions02.jpg",
             heading: "Step 2",
             description:
-                "Tighten your thigh muscles and slowly extend the selected leg upward as high as you comfortably can."),
+                "Tighten your thigh muscles and slowly extend the selected leg upward as high as you comfortably can.",
+            function: () {  },
+        ),
         TutorialStep(
             assetURI: "assets/knee_rehab/leg_extensions03.jpg",
             heading: "Step 3",
             description:
-                "Hold the leg fully extended and keep your thigh muscles engaged for 3 seconds. Then relax, lower your foot back to the floor, and rest for 3 seconds. Repeat this 10 times."),
+                "Hold the leg fully extended and keep your thigh muscles engaged for 3 seconds. Then relax, lower your foot back to the floor, and rest for 3 seconds. Repeat this 10 times.",
+            function: () {  },
+        ),
         TutorialStep(
             assetURI: "assets/knee_rehab/something.png",
             heading: "Important!",
             description:
-                "Avoid swinging your leg or using forceful movements. Try to lift and lower your leg in a straight line, using slow and controlled motion both ways."),
+                "Avoid swinging your leg or using forceful movements. Try to lift and lower your leg in a straight line, using slow and controlled motion both ways.",
+            function: () {  },
+        ),
       ],
     ),
     /// Arm rehab    
-    ExerciseSelectView(icon: Icons.accessibility_new, title: "Arm rehabilitation",
+    ExerciseSelectView(icon: Icons.waving_hand, title: "Arm rehabilitation",
       tutorialSteps: [
         TutorialStep(
           assetURI: "assets/arm_rehab/images/armband.jpg",
@@ -199,7 +224,6 @@ class MenuViewModel extends ChangeNotifier {
         ),
       ],
     ),
-    
     /// Settings
     SettingsView(icon: Icons.settings, title: "Settings"),
   ];
